1fe4f7e80c905957d50d7bb98121e255fcdcef92
ikechan8370 <geyinchi@buaa.edu.cn>
2023-07-16 18:27:28 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+// const {HttpsProxyAgent} = require('https-proxy-agent');
+
+// HTTP Proxy
+// const httpProxyUrl = 'http://127.0.0.1:7890';
+// const socks5Agent = new HttpsProxyAgent(httpProxyUrl);
+
+// Create Express app
+const app = express();
+const socketProxy = createProxyMiddleware({
+    target: 'https://sydney.bing.com',
+    pathFilter: '/sydney/ChatHub',
+    ws: true,
+    changeOrigin: true,
+    // agent: socks5Agent,
+    headers: {
+        origin: 'https://www.bing.com',
+        Referer: 'https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx',
+        'x-forwarded-for': generateRandomIP(),
+    }
+});
+// Proxy WebSocket requests
+app.use('/sydney/ChatHub', socketProxy);
+
+// Start the server
+const port = 3000;  // Replace with your desired port number
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+function generateRandomIP () {
+    const baseIP = '20.136.128'
+    const subnetSize = 254 // 2^8 - 2
+    const randomIPSuffix = Math.floor(Math.random() * subnetSize) + 1
+    let ip = baseIP + randomIPSuffix
+    return ip
+}
