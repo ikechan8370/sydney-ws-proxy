b3b84ce8d40fd308c60d2a62ef657b1eafa2701c
ikechan8370 <geyinchi@buaa.edu.cn>
2023-07-21 23:48:01 +0800

    feat: 验证码机制

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 // const {HttpsProxyAgent} = require('https-proxy-agent');
+const axios = require("axios");
 
 // HTTP Proxy
 // const httpProxyUrl = 'http://127.0.0.1:7890';
@@ -49,6 +50,38 @@ const imageProxy = createProxyMiddleware({
 app.use('/sydney/ChatHub', socketProxy);
 app.use('/turing/conversation/create', createConversationProxy)
 app.use('/images/create', imageProxy)
+app.use('/edgesvc/turing/captcha/create', async (req, res) => {
+    let result = await axios.get("https://edgeservices.bing.com/edgesvc/turing/captcha/create", {
+        responseType: 'arraybuffer',
+        headers: {
+            Cookie: req.headers.cookie,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82',
+            Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        },
+        // httpsAgent: socks5Agent,
+    })
+    const headers = result.headers;
+    Object.keys(headers).forEach((headerName) => {
+        res.set(headerName, headers[headerName]);
+    });
+    res.send(result.data);
+})
+app.use('/edgesvc/turing/captcha/verify', async (req, res) => {
+    let result = await axios.get("https://edgeservices.bing.com/edgesvc/turing/captcha/verify", {
+        params: req.query,
+        headers: {
+            Cookie: req.headers.cookie,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82',
+            Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        },
+        // httpsAgent: socks5Agent,
+    })
+    const headers = result.headers;
+    Object.keys(headers).forEach((headerName) => {
+        res.set(headerName, headers[headerName]);
+    });
+    res.send(result.data);
+})
 
 // Start the server
 const port = 3000;  // Replace with your desired port number
